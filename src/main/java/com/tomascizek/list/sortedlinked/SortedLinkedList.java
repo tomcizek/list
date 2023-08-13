a85abdb6a8074a8cf2d12ab7a082402e229994e2
Tomáš Čížek <tomascizek@gmail.com>
2023-08-13 19:46:01 +0200

    SortedLikedList - it can insert single element

--- src/main/java/com/tomascizek/list/sortedlinked/SortedLinkedList.java
+++ src/main/java/com/tomascizek/list/sortedlinked/SortedLinkedList.java
@@ -1,7 +1,14 @@
 package com.tomascizek.list.sortedlinked;
 
 public class SortedLinkedList<T> {
+  
+  private int size = 0;
+  
   public int size() {
-    return 0;
+    return this.size;
+  }
+
+  public void insert(T apple) {
+    this.size++;
   }
 }
